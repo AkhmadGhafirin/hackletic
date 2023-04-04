67c18eaf8edb40b1e980f6e882156fcabfdb8053
AkhmadGhafirin <ahmadghafirin@gmail.com>
2023-04-04 08:47:33 +0700

    testing commit

---
+++ index.js
@@ -0,0 +1,52 @@
+function deleteNews(id) {
+
+}
+
+function readNews(id) {
+
+}
+
+function updateNews(img, title, desc) {
+
+}
+
+function createNews(img, title, desc) {
+
+}
+
+function showNews(data) {
+    
+}
+
+function main(action, img, title, desc) {
+
+}
+
+let data = [
+    {
+        title: 'Lorem ipsum',
+        desc: 'Lorem ipsum dolor',
+        img: 'https://cdn.dribbble.com/userupload/3529806/file/original-370120c1b24eb2323c9c0a2a8dd4bf80.jpg?compress=1&resize=1200x900'
+    },
+    {
+        title: 'Lorem ipsum',
+        desc: 'Lorem ipsum dolor',
+        img: 'https://cdn.dribbble.com/userupload/3529806/file/original-370120c1b24eb2323c9c0a2a8dd4bf80.jpg?compress=1&resize=1200x900'
+    },
+    {
+        title: 'Lorem ipsum',
+        desc: 'Lorem ipsum dolor',
+        img: 'https://cdn.dribbble.com/userupload/3529806/file/original-370120c1b24eb2323c9c0a2a8dd4bf80.jpg?compress=1&resize=1200x900'
+    },
+    {
+        title: 'Lorem ipsum',
+        desc: 'Lorem ipsum dolor',
+        img: 'https://cdn.dribbble.com/userupload/3529806/file/original-370120c1b24eb2323c9c0a2a8dd4bf80.jpg?compress=1&resize=1200x900'
+    },
+    {
+        title: 'Lorem ipsum',
+        desc: 'Lorem ipsum dolor',
+        img: 'https://cdn.dribbble.com/userupload/3529806/file/original-370120c1b24eb2323c9c0a2a8dd4bf80.jpg?compress=1&resize=1200x900'
+    }
+]
+
